d6e5f0efcc6dff961fe5b5aa87b4c7c8dc9a8ec0
DeeMATT <mattode@outlook.com>
2019-09-26 17:30:15 +0100

    view function to add a new ToDo item and a decorator to ensure the function accepts only POST request

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
 from .models import Todo
 from .forms import TodoForm
 
@@ -8,6 +10,13 @@ def index(request):
     context = {'todo_list': todo_list, 'form': form}
     return render(request, 'ToDo/index.html', context)
 
+@require_POST
 def addTodo(request):
-    form = TodoForm()
+    form = TodoForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save() 
+
+    return redirect('index')
     
